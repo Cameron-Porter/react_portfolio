743f19773f62940a734433f1da3b940e7082acc0
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-06-15 16:10:34 -0400

    Updated blog posts as cards

---
+++ src/components/Paperstack.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Paperstacks() {
+  return (
+    <div class="cards">
+      <div class="cards-inner back"></div>
+      <div class="cards-inner"></div>
+      <div class="cards-inner front">
+        <p class="r-text">So Many Options!</p>
+        <ul class="r-list">
+          <li>School</li>
+          <li>FreeCodeCamp</li>
+          <li>YouTube</li>
+          <li>Bootcamp</li>
+          <li>etc</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Paperstacks;
--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -6,7 +6,7 @@ function Blog() {
     <div className="home mx-3">
       <div class="container">
         <h1 className="text-center mt-5">D.A.D. Chats</h1>
-        <p class="dad text-center">(Dedicated & Devoted)</p>
+        <p class="dad text-center mb-5">(Dedicated & Devoted)</p>
         <Outlet />
       </div>
     </div>
